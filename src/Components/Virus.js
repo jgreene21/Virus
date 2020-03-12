51b16a341e7477d124964ba1a0b022c35acffc99
Charles Buxton <chazbux@gmail.com>
2020-03-12 15:55:09 -0600

    stable cards

--- src/Components/Virus.js
+++ src/Components/Virus.js
@@ -1,26 +1,67 @@
 import React from 'react'
 import NewVirus from './NewVirus'
-import VirusCard from './Card'
+import VirusCard from './VirusCard'
 
 
 class Virus extends React.Component {
 
   state = {
     cards: [
-      { id: 1, front: "Card Front", back: "Card Back",  flipped: false },
-      { id: 2, front: "Card Front 2", back: "Card Back 2",  flipped: false },
-      { id: 3, front: "Card Front 3", back: "Card Back 3",  flipped: false }
+      { id: 1, front: "Virus 1", back: "Virus 1 Back",  flipped: false },
+      { id: 2, front: "Virus 2", back: "Virus Back 2",  flipped: false },
+      { id: 3, front: "Virus 3", back: "Virus Back 3",  flipped: false }
     ],
     showForm: false
   }
   
+  toggleForm = () => {
+    // nice easy way to toggle element
+    this.setState({
+      showForm: !this.state.showForm
+    })
+
+  }
+  newCard = (card) => {
+    //called by handleSubmit function in form
+    console.log(card)
+    let newArr = [card, ...this.state.cards]
+
+    this.setState({
+      cards: newArr
+    });
+  }
+
+  deleteCard = (id) => {
+    let { cards } = this.state
+    let filteredArr = cards.filter(card => card.id !== id);
+
+    this.setState({
+      cards: filteredArr
+    });
+  }
+
+  flipCard = (id) => {
+    let { cards } = this.state
+
+
+    //locate object:
+    let arrFlip = cards.map(card => {
+      if (card.id === id) {
+        return { id: card.id, front: card.front, back: card.back, study: card.study, flipped: card.flipped = !card.flipped }
+      }
+      return card
+    });
+    this.setState({
+      cards: arrFlip
+    });
+  } 
 
 render() {
   return (
     
     <div>
       <NewVirus/>
-      <VirusCard/>
+      <VirusCard cards={this.state.cards} deleteCard={this.deleteCard} flipCard={this.flipCard}/>
     </div>
   )
 }
